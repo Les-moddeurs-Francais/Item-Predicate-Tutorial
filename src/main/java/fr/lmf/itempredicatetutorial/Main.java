4932b61fdc0222846b0e0c67f1d3a6609b1fc2e4
Mysterious_Dev <mysteriousdevs@gmail.com>
2021-07-25 21:32:14 +0200

    1.17 Update

--- src/main/java/fr/lmf/itempredicatetutorial/Main.java
+++ src/main/java/fr/lmf/itempredicatetutorial/Main.java
@@ -1,34 +1,18 @@
 package fr.lmf.itempredicatetutorial;
 
 import fr.lmf.itempredicatetutorial.init.ItemInit;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.item.ElytraItem;
-import net.minecraft.item.ItemModelsProperties;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Main.MODID)
 public class Main {
 
     public static final String MODID = "item_predicate_tutorial";
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public Main() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
@@ -39,8 +23,8 @@ public class Main {
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
-        ItemModelsProperties.registerProperty(ItemInit.TEST_PREDICATE.get(), new ResourceLocation("test"), (p_239423_0_, p_239423_1_, p_239423_2_) -> {
-            return p_239423_0_.getTag() != null && p_239423_0_.getTag().getBoolean("isBest") ? 1.0F : 0.0F;
+        ItemProperties.register(ItemInit.TEST_PREDICATE.get(), new ResourceLocation("test"), (tag, p_174631_, p_174632_, p_174633_) -> {
+            return tag.getTag() != null && tag.getTag().getBoolean("isBest") ? 1.0F : 0.0F;
         });
     }
 }
--- src/main/java/fr/lmf/itempredicatetutorial/init/ItemInit.java
+++ src/main/java/fr/lmf/itempredicatetutorial/init/ItemInit.java
@@ -1,9 +1,9 @@
 package fr.lmf.itempredicatetutorial.init;
 
 import fr.lmf.itempredicatetutorial.Main;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraftforge.fml.RegistryObject;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -11,6 +11,6 @@ public class ItemInit {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MODID);
 
-    public static final RegistryObject<Item> TEST_PREDICATE = ITEMS.register("test_predicate", ()-> new Item((new Item.Properties()).group(ItemGroup.MATERIALS)));
+    public static final RegistryObject<Item> TEST_PREDICATE = ITEMS.register("test_predicate", ()-> new Item((new Item.Properties()).tab(CreativeModeTab.TAB_MATERIALS)));
 
 }
